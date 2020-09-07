1a0091d18c515324a2c33d14fce77e1e539caab9
maryam-azizi <azizi.m1390@gmail.com>
2020-09-07 14:55:33 +0430

    ex6 indexOf

---
+++ ex6-indexof/main.js
@@ -0,0 +1,52 @@
+function checkEmail(email){
+if (email.indexOf('@[') == -1){
+	console.log ('wrong email');
+	}  else{
+		alert('corret');
+		}
+}
+function submit(){
+	var sentence = document.querySelector('#sentence').value;
+	var search = document.querySelector('#search').value;
+	var i = sentence.indexOf(search);
+	var hasErrors = false;
+	if (search.trim().length == 0){
+		  addDangerAlert('Search is empty'); 
+		  hasErrors = true;
+		}
+	if (sentence.trim().length == 0){
+		 addDangerAlert('Sentence is empty');
+		 hasErrors = true; 
+	}
+	
+	if (hasErrors) return;
+	
+	if (i == -1){
+		document.querySelector('#result').innerHTML= "Search not found";
+	}
+	else{
+			var output = boldSearch(sentence,search);
+    
+			document.querySelector('#result').innerHTML= output;
+		}
+}
+function boldSearch(sentence,search){
+		var i = sentence.indexOf(search);
+		if (i== -1)return sentence;
+		var output = sentence.substring(0,i)+'<strong>'+search+ '</strong>'+boldSearch(sentence.substring(i+search.length),search);
+		return output;
+}
+function addEvent(){
+	document.querySelector('#btn-search').addEventListener('click',submit);
+}
+
+function addDangerAlert(massage){
+		var alertHtml = '<div class="alert alert-danger alert-dismissable alert" role="alert">'+
+		massage +'<button type="button" class="close" data-dismiss="alert"> X </button></div>';
+		document.querySelector('#result').innerHTML += alertHtml;
+}
+
+function init(){
+		addEvent();
+}
+init();
